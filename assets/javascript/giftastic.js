c58159b3792a82dbf3ca41ff6f0442b8b7555daa
Baruch Hashem <barukhashem@gmail.com>
2019-01-30 12:10:01 -0700

    latest

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -18,135 +18,159 @@
 
 // Under every gif, display its rating (PG, G).
 
-// This data is provided by the GIPHY API.
-// !!!! Only once images are displaying with button presses, then move to the next step.
+// Add a form to the page that takes the value from a user input box and adds it into the topics array.
 
-// Add a form to the page that takes the value from a user input box and adds it into the topics array. Then create a function call that takes each topic in the array and renders the buttons on the page.
+// Then create a function call that takes each topic in the array and renders the buttons on the page.
 
 
 
 // BEGIN CODING HERE:
 
-// Creates an array of superheroes:
-var superheroes = ["Superman", "Wonder Woman", "Batman", "Aquaman", "Green Lantern", "The Flash", "Iron Man", "Thor", "Captain America", "Dr. Strange", "The Hulk", "Spiderman", "Wolverine", "X-Men", "Deadpool", "Fantastic Four", "Guardians of the Galaxy"];
+// This creates an array of superheroes:
+var superheroes = ["Superman", "Wonder Woman", "Batman", "Aquaman", "Green Lantern", "The Flash", "Iron Man", "Thor", "Captain America", "Dr. Strange", "The Hulk", "Spiderman", "Wolverine", "X-Men", "Deadpool"];
 
-// Creates a function to create gifs for each superhero:
+// This creates a function to create gifs for each superhero:
 function displaySuperheroGifs() {
 
-    // handles events:
+    // This handles events:
     var superhero = $(this).attr("data-name");
 
-    // sets query to giphy's api:
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + superhero + "&limit=10&rating=g&rating=pg&api_key=6XGYTHgYrJJWFYwVAXX2TAM5LFzeuyvw";
+    // This creates a query to giphy.com's api:
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + superhero + "&limit=12&rating=g&rating=pg&api_key=6XGYTHgYrJJWFYwVAXX2TAM5LFzeuyvw";
 
-    // Creates an AJAX call for the specific superhero button being clicked:
+    // This creates an AJAX call for the specific superhero button being clicked:
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+
+        // This creates a variable called "data" for the response data from the ajax call:
         var data = response.data;
         console.log(data);
+
+        // This loops through the data array from the ajax call:
         for (var i = 0; i < data.length; i++) {
+
+            // This creates a variable called "gif" for the response data from the ajax call: 
             var gif = response.data[i];
 
-            // Creates a div to hold the gif:
-            var superheroDiv = $("<div class='superhero'>");
+            // This creates a div to hold the gif and sets the column class:
+            var superheroDiv = $("<div class='col-12 col-md-4'>");
 
-            // Stores the rating data:
+            // This stores the rating data:
             var rating = data[i].rating;
             console.log(rating)
 
-            // Creates an element to have the rating displayed:
+            // This creates an element to have the rating displayed:
             var p = $("<p>").text("Rating: " + rating);
 
-            // Displays the rating:
+            // This displays the rating:
             superheroDiv.append(p);
 
-            // var div = $("<div class='col-12 col-md-6'>").append(p, image);
-
-            // Retrieves the URL for the image:
+            // This retrieves the URL for the image:
             var imgURL = response.data.image_original_url;
 
-            // Creates an element to hold the image:
+            // This creates an element to hold the image:
             var image = $("<img>");
+
+            // This maps the image's src path:
             image.attr("src", data[i].images.fixed_width_still.url);
+
+            // This maps the image's data-still path:
             image.attr("data-still", data[i].images.fixed_width_still.url);
+
+            // This maps the image's data-animate path:
             image.attr("data-animate", data[i].images.fixed_width.url);
+
+            // This sets the image's data-state as "still":
             image.attr("data-state", "still");
+
+            // This sets the image's class as "gifs":
             image.attr("class", "gifs");
+
+            // This adds the image to the bottom of the superheroDiv:
             superheroDiv.append(image)
 
-            // Calls renderButtons which handles the processing of our superhero array:
+            // This calls superheroes-view which handles the processing of our superhero array:
             $("#superheroes-view").append(superheroDiv)
         }
     });
 }
 
-// Creates a button for each superhero:
+// This creates a button for each superhero:
 function renderButtons() {
 
-    // Deletes any existing superheroes prior to adding new superheroes to avoid duplicate buttons:
+    // This deletes any existing superheroes prior to adding new superheroes to avoid duplicate buttons:
     $("#buttons-view").empty();
 
-    // Loops through the superheroes array:
+    // This loops through the superheroes array:
     for (var i = 0; i < superheroes.length; i++) {
-        // console.log(superheroes);
-        // Then dynamicaly generating buttons for each superhero in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        
+        // Then it dynamically generates buttons for each superhero in the array.
+
+        // This creates the button's beginning and end tag (<button></button>):
         var a = $("<button>");
-        // Adding a class of superhero-btn to our button:
+
+        // This adds the class of superhero-btn to the superhero button:
         a.addClass("superhero-btn");
-        // Adding a data-attribute:
+
+        // This adds a data-attribute:
         a.attr("data-name", superheroes[i]);
-        // Providing the initial button text:
+
+        // This provides the initial button text:
         a.text(superheroes[i]);
-        // Adding the button to the buttons-view div:
+
+        // This adds the button to the bottom of the buttons-view div:
         $("#buttons-view").append(a);
     }
 }
+
+// This executes the renderButtons function which handles the processing of the superherooes array:
 renderButtons();
 
 // This function handles events where a superhero button is clicked:
 $("#add-superhero").on("click", function (event) {
+
     // event.preventDefault() prevents the form from trying to refresh or submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want:
+    // Here we're using a form so that the user can press "Enter" instead of clicking the "Add Superhero" button if they want:
     event.preventDefault();
-    // This line grabs the input from the textbox:
+
+    // This line takes the input from the textbox:
     var superhero = $("#superhero-input").val().trim();
 
-    // Adds superhero from the textbox to our array:
+    // This adds superhero from the textbox to our array:
     superheroes.push(superhero);
 
-    // Calls renderButtons which handles the processing of our superhero array:
+    // This executes the renderButtons function which handles the processing of the superherooes array:
     renderButtons();
 });
 
-// This function handles events where a movie button is clicked
+// This on-click function handles events where a superhero button is clicked:
 $(document).on("click", ".superhero-btn", displaySuperheroGifs);
 
+// This on-click function handles events where a superhero gif is clicked:
 $(document).on("click", ".gifs", function () {
     //     var $this = $(this);
 
-    // Make a variable named state and then store the image's data-state into it.
-    // Use the .attr() method for this.
+    // This creates a variable called "state" and then stores the image's data-state into it and then uses the .attr() method for "this."
     var state = $(this).attr("data-state");
 
-    // Check if the variable state is equal to 'still',
+    // This checks if the variable "state" is equal to "still":
     if (state === "still") {
 
-        // then update the src attribute of this image to its data-animate value:
+        // Then it updates the src attribute of this image to its data-animate value:
         $(this).attr("src", $(this).attr("data-animate"));
 
-        // then update the data-state attribute to "animate":
+        // This updates the data-state attribute to "animate":
         $(this).attr("data-state", "animate");
 
-        // If state is equal to 'animate':
+        // If state is equal to "animate":
     } else if (state === "animate") {
 
-        // then update the src attribute of this image to its data-still value:
+        // Then it updates the src attribute of this image to its data-still value:
         $(this).attr("src", $(this).attr("data-still"));
 
-        // and update the data-state attribute to "still":
+        // This updates the data-state attribute to "still":
         $(this).attr("data-state", "still");
     }
 })
